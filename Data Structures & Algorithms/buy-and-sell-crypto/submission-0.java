ebb54c7e170cb3f26a97b04082a3f7018f4a3b25
salinirajadas-beghin <salinirajadas@gmail.com>
2026-07-21 21:46:11 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int price = prices[0];
+        int profit = 0;
+        for(int i=0;i<prices.length;i++) {
+            if(prices[i] < price) {
+                 price = prices[i];
+            } 
+            profit = Math.max(profit , prices[i]-price);
+        }
+        return profit ;
+    }
+}
